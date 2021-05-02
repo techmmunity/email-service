e5ac7d92ac25ec6caa4f2232f446b5bf66df3b03
Henrique Leite <henriqueleite616@gmail.com>
2021-05-02 17:42:34 -0300

    Add Template Entity

---
+++ src/v1/api/template/entities/template.entity.ts
@@ -0,0 +1,61 @@
+import {
+	BaseEntity,
+	Column,
+	Entity,
+	CreateDateColumn,
+	Repository,
+	FindManyOptions,
+	FindOneOptions,
+	PrimaryColumn,
+	Index,
+	UpdateDateColumn,
+	Unique,
+} from "typeorm";
+
+import { ApplicationEnum, ApplicationValues } from "core/enums/applications";
+
+import { Limits } from "v1/config/limits";
+
+import { DefaultOmitEntityFields } from "types/entity";
+
+@Entity("templates")
+@Unique(["application", "code"])
+export class TempalteEntity extends BaseEntity {
+	@PrimaryColumn({
+		length: Limits.ids.uuid.length,
+	})
+	public id: string;
+
+	@Column({
+		nullable: false,
+		enum: ApplicationValues(),
+	})
+	public application: ApplicationEnum;
+
+	@Index()
+	@Column({
+		length: Limits.template.code.max,
+		nullable: false,
+	})
+	public code: string;
+
+	@CreateDateColumn({
+		name: "created_at",
+		nullable: false,
+	})
+	public createdAt: Date;
+
+	@UpdateDateColumn({
+		name: "updated_at",
+		nullable: false,
+	})
+	public updatedAt: Date;
+}
+
+export type TempalteType = Omit<TempalteEntity, DefaultOmitEntityFields>;
+
+export type TempalteRepository = Repository<TempalteEntity>;
+
+export type TempalteFindMany = FindManyOptions<TempalteEntity>;
+
+export type TempalteFindOne = FindOneOptions<TempalteEntity>;
---
+++ src/v1/config/limits.ts
@@ -0,0 +1,13 @@
+export const Limits = {
+	ids: {
+		uuid: {
+			length: 36,
+		},
+	},
+	template: {
+		code: {
+			min: 3,
+			max: 25,
+		},
+	},
+};
