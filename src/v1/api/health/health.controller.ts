f83e32d360f7ec7d5d3c913e4a5445f652d95007
Williancc1557 <minegt1500@gmail.com>
2021-06-01 20:15:07 -0300

    bug consertado

--- src/v1/api/health/health.controller.ts
+++ src/v1/api/health/health.controller.ts
@@ -10,11 +10,7 @@ import { ApiHealthIndicator } from "./health.indicator";
 
 import { ApiConfig } from "v1/config";
 
-<<<<<<< HEAD
 @ApiTags(`${ApiConfig.version} - Health`)
-=======
-@ApiTags(`${ApiConfig.version} - health`)
->>>>>>> 1010b2a46a34eeb5fb1e3777dda01bfe8fe62c89
 @Controller(`${ApiConfig.version}/health`)
 export class HealthController {
 	public constructor(
